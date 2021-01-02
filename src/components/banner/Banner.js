585d249c2a33433347e9b44ef2ad603c249c462b
Loida Jane Anfone <71513001+its-el-jey@users.noreply.github.com>
2021-01-02 19:54:43 +0800

    banner component

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -4,12 +4,54 @@ import requests from '../../request';
 import axios from '../../axios';
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
 
+  useEffect(() => {
 
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  // truncate means shortened the certain element
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
 
   return (
-    <header className="banner">
+    <header className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+        "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+        )`,
+        backgroundPosition: "center center"
+      }}
+    >
 
+    <div className="banner__contents">
+      <h1 className="banner__title">
+      {/* if movie.title is not exist find movie.name/original */}
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+
+      <div className="banner__buttons">
+        <button className="banner__button">Play</button>
+        <button className="banner__button">My List</button>
+      </div>
+      
+      {/* truncate into 150 characters ... */}
+      <h1 className="banner__description"> {truncate(movie?.overview, 150)} </h1>
+    </div>
+
+    <div className="banner__fadeBottom" />
     </header>
   )
 }
